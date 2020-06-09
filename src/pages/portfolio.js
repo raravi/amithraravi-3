91d418d0400d23467942f5995608f5767cb1dcae
Amith Raravi <amith.raravi@gmail.com>
2020-06-09 02:35:34 +0200

    Added Portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,15 +1,76 @@
 import React from 'react';
-import { Link } from 'gatsby';
-
+import { useStaticQuery, graphql } from 'gatsby';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import styles from '../styles/portfolio.module.css';
 
-const PortfolioPage = () => (
-  <Layout>
-    <SEO title="Portfolio" />
-    <p>Portfolio</p>
-    <Link to="/">Go home</Link>
-  </Layout>
-);
+const PortfolioPage = () => {
+  const { site, portfolio } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            siteUrl
+          }
+        }
+        portfolio: allMarkdownRemark(
+          sort: {
+            fields: [frontmatter___date]
+            order: DESC
+          }
+          filter: {
+            fields: {
+              sourceName: {eq: "portfolio"}
+            }
+          }
+        ) {
+          nodes {
+            id
+            frontmatter {
+              title
+              teaser
+            }
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    `,
+  );
+
+  return (
+    <Layout>
+      <SEO title="Portfolio" />
+      <div className={styles.portfolioHeader}>
+        <div className={styles.portfolioHeader_pageTitle}>
+          <h2>Portfolio</h2>
+        </div>
+      </div>
+      <div className="div-5-high" />
+      <div className={styles.portfolioTilesGrid}>
+        {
+          portfolio.nodes.map((node) => (
+            <article className={`${styles.portfolioTile} ${styles.col}`}>
+              <a
+                href={`${site.siteMetadata.siteUrl}/portfolio/${node.fields.slug.slice(12)}`}
+                title={node.frontmatter.title}
+                className={styles.portfolioLink}
+              >
+                <p className={styles.postTeaser}>
+                  <img
+                    src={`/images/${node.frontmatter.teaser}`}
+                    alt="teaser"
+                  />
+                </p>
+                <h2 className={styles.postTitle}>{node.frontmatter.title}</h2>
+              </a>
+            </article>
+          ))
+        }
+      </div>
+    </Layout>
+  );
+};
 
 export default PortfolioPage;
